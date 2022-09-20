b29f0cd8bac52caf0bb4089798ef5600324d68ad
Your Name <joelebell@gmail.com>
2022-09-20 00:13:59 -0400

    Here is my homework

---
+++ 02-Challenge/js/questions.js
@@ -0,0 +1,34 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is an array?",
+    answer: "An array is a special variable, which can hold more than one value",
+    options: [
+      "An array is a special variable, which can hold more than one value",
+      "An array is a special variable, which can hold only one value",
+      "An array is a special variable, which does not hold value",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are varibles?",
+    answer: "Variables are containers for storing data",
+    options: [
+      "Variables are strings",
+      "Variables are containers for storing data",
+      "Variables are not containers ",
+    ]
+  },
+    {
+    numb: 3,
+    question: "How do you comment with Javascript?",
+    answer: "//",
+    options: [
+      "==",
+      "??",
+      "//",
+    ]
+  },
+];
+
+
